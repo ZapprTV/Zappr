184f900fffd89353df945b6e10ec3a70d7f5d75d
Francesco <hi@francescoro.si>
2025-02-05 22:09:06 +0100

    ops mi so perso offline-pwa

--- build.mjs
+++ build.mjs
@@ -30,7 +30,7 @@ await fs.promises.rm(joinPath(process.cwd(), "dist"), { recursive: true, force: 
 
 console.log("bundling in corso...");
 let bundler = new Parcel({
-    entries: "index.html",
+    entries:  ["index.html", "offline-pwa.html"],
     defaultConfig: "@parcel/config-default",
     mode: "production"
 });

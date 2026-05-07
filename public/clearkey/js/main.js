06c054eb4ac66c43f27d8066d2876f3447317cfd
Francesco <52544979+FrancescoRosi@users.noreply.github.com>
2026-05-07 21:10:54 +0200

    aggiunto supporto per stream con molteplici key nel player ClearKey

--- public/clearkey/js/main.js
+++ public/clearkey/js/main.js
@@ -1,12 +1,20 @@
+const url = new URL(location.href).searchParams.get("url");
+const additionalParams = Object.fromEntries(Object.entries(Object.fromEntries(new URL(url).searchParams)).filter(([key]) => key.startsWith("clearkeyPlayer.")));
 const kid = new URL(location.href).searchParams.get("kid");
 const key = new URL(location.href).searchParams.get("key");
-const url = new URL(location.href).searchParams.get("url");
+const keys = JSON.parse(decodeURIComponent(new URL(location.href).searchParams.get("keys")));
 
 if (url) {
     document.addEventListener("shaka-ui-loaded", async () => {
         const video = document.querySelector('video');
         const player = video.ui.getControls().getPlayer();
-        if (kid && key) {
+        if (keys) {
+            player.configure({
+                drm: {
+                    clearKeys: keys
+                }
+            });
+        } else if (kid && key) {
             player.configure({
                 drm: {
                     clearKeys: {
@@ -15,6 +23,15 @@ if (url) {
                 }
             });
         };
+
+        if (additionalParams["clearkeyPlayer.maxQuality"]) {
+            player.configure({
+                restrictions: {
+                    maxHeight: parseInt(additionalParams["clearkeyPlayer.maxQuality"])
+                }
+            });
+        };
+
         await player.load(url);
 
         window.player = player;

ee2e0b481ac313dbf0b61a5bd3e89e755dccb70b
Francesco <52544979+FrancescoRosi@users.noreply.github.com>
2026-05-19 17:21:58 +0200

    aggiunto parametro per linguaggio audio nel player ClearKey

--- public/clearkey/js/main.js
+++ public/clearkey/js/main.js
@@ -1,10 +1,14 @@
 const url = new URL(location.href).searchParams.get("url");
-const additionalParams = Object.fromEntries(Object.entries(Object.fromEntries(new URL(url).searchParams)).filter(([key]) => key.startsWith("clearkeyPlayer.")));
+const urlWithoutSearch = url.replace(new URL(url).search, "");
+const searchParams = Object.entries(Object.fromEntries(new URL(url).searchParams));
+const playerOptions = Object.fromEntries(searchParams.filter(([key]) => key.startsWith("clearkeyPlayer.")));
+const nonPlayerSearch = Object.fromEntries(searchParams.filter(([key]) => !key.startsWith("clearkeyPlayer.")));
+const finalURL = `${urlWithoutSearch}?${new URLSearchParams(nonPlayerSearch).toString()}`;
 const kid = new URL(location.href).searchParams.get("kid");
 const key = new URL(location.href).searchParams.get("key");
 const keys = JSON.parse(decodeURIComponent(new URL(location.href).searchParams.get("keys")));
 
-if (url) {
+if (finalURL) {
     document.addEventListener("shaka-ui-loaded", async () => {
         const video = document.querySelector('video');
         const player = video.ui.getControls().getPlayer();
@@ -24,15 +28,17 @@ if (url) {
             });
         };
 
-        if (additionalParams["clearkeyPlayer.maxQuality"]) {
+        if (playerOptions["clearkeyPlayer.maxQuality"]) {
             player.configure({
                 restrictions: {
-                    maxHeight: parseInt(additionalParams["clearkeyPlayer.maxQuality"])
+                    maxHeight: parseInt(playerOptions["clearkeyPlayer.maxQuality"])
                 }
             });
         };
+        
+        await player.load(finalURL);
 
-        await player.load(url);
+        if (playerOptions["clearkeyPlayer.audioLanguage"] && player.getAudioLanguages().includes(playerOptions["clearkeyPlayer.audioLanguage"])) player.selectAudioLanguage(playerOptions["clearkeyPlayer.audioLanguage"]);
 
         window.player = player;
     });
